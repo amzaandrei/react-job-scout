a0b5e6a698dc9f6a70498e41b41128f96b6b95b9
Andrei Amza <andrei.amza177@gmail.com>
2025-01-10 13:32:05 +0000

    add: config, entrypoint, and package init

---
+++ src/react_job_scout/__init__.py
@@ -0,0 +1,15 @@
+
+# Unsupported bytecode in file /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/__pycache__/__init__.cpython-314.pyc
+# Unsupported Python version, 3.14, for decompilation
+
+# Unsupported Python version, 3.14, for decompilation
+# Can't uncompile /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/__pycache__/__init__.cpython-314.pyc
+# uncompyle6 version 3.9.3
+# Python bytecode version base 3.14 (3627)
+# Decompiled from: Python 3.14.3 (main, Feb  3 2026, 15:32:20) [Clang 17.0.0 (clang-1700.6.3.2)]
+# Embedded file name: /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/__init__.py
+# Compiled at: 2026-05-09 16:20:15
+# Size of source mod 2**32: 284 bytes
+
+Unsupported Python version, 3.14, for decompilation
+
