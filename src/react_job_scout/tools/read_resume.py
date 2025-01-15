d864c9cf3ee6904cdc2ba6a731f7fe552f29072b
Andrei Amza <andrei.amza177@gmail.com>
2025-01-15 10:07:44 +0000

    add: tool definitions (search, fetch, score, cover letter, resume)

---
+++ src/react_job_scout/tools/read_resume.py
@@ -0,0 +1,15 @@
+
+# Unsupported bytecode in file /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/tools/__pycache__/read_resume.cpython-314.pyc
+# Unsupported Python version, 3.14, for decompilation
+
+# Unsupported Python version, 3.14, for decompilation
+# Can't uncompile /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/tools/__pycache__/read_resume.cpython-314.pyc
+# uncompyle6 version 3.9.3
+# Python bytecode version base 3.14 (3627)
+# Decompiled from: Python 3.14.3 (main, Feb  3 2026, 15:32:20) [Clang 17.0.0 (clang-1700.6.3.2)]
+# Embedded file name: /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/tools/read_resume.py
+# Compiled at: 2026-05-09 16:22:37
+# Size of source mod 2**32: 2015 bytes
+
+Unsupported Python version, 3.14, for decompilation
+
---
+++ src/react_job_scout/tools/search_jobs.py
@@ -0,0 +1,15 @@
+
+# Unsupported bytecode in file /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/tools/__pycache__/search_jobs.cpython-314.pyc
+# Unsupported Python version, 3.14, for decompilation
+
+# Unsupported Python version, 3.14, for decompilation
+# Can't uncompile /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/tools/__pycache__/search_jobs.cpython-314.pyc
+# uncompyle6 version 3.9.3
+# Python bytecode version base 3.14 (3627)
+# Decompiled from: Python 3.14.3 (main, Feb  3 2026, 15:32:20) [Clang 17.0.0 (clang-1700.6.3.2)]
+# Embedded file name: /Users/andreiamza/Developer/react-job-scout/src/react_job_scout/tools/search_jobs.py
+# Compiled at: 2026-05-09 16:22:13
+# Size of source mod 2**32: 3797 bytes
+
+Unsupported Python version, 3.14, for decompilation
+
