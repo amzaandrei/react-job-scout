8a3e0099d7f4d24fe3ceba61628efac730db378a
Andrei Amza <andrei.amza177@gmail.com>
2025-01-28 09:22:33 +0000

    add: test suite for agent and tools

---
+++ tests/test_agent.py
@@ -0,0 +1,15 @@
+
+# Unsupported bytecode in file /Users/andreiamza/Developer/react-job-scout/tests/__pycache__/test_agent.cpython-314-pytest-9.0.3.pyc
+# Unsupported Python version, 3.14, for decompilation
+
+# Unsupported Python version, 3.14, for decompilation
+# Can't uncompile /Users/andreiamza/Developer/react-job-scout/tests/__pycache__/test_agent.cpython-314-pytest-9.0.3.pyc
+# uncompyle6 version 3.9.3
+# Python bytecode version base 3.14 (3627)
+# Decompiled from: Python 3.14.3 (main, Feb  3 2026, 15:32:20) [Clang 17.0.0 (clang-1700.6.3.2)]
+# Embedded file name: /Users/andreiamza/Developer/react-job-scout/tests/test_agent.py
+# Compiled at: 2026-05-09 16:24:55
+# Size of source mod 2**32: 5253 bytes
+
+Unsupported Python version, 3.14, for decompilation
+
